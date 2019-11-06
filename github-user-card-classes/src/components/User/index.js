5ed7b604358af1b67751138a10d68a9eedbe24ea
Jason Barr <jason@jasonsbarr.com>
2019-11-06 09:49:58 -0600

    Rearrange elements to make card

--- github-user-card-classes/src/components/User/index.js
+++ github-user-card-classes/src/components/User/index.js
@@ -1,24 +1,34 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import styles from "./styles";
 
 const User = ({ user }) => {
   return (
-    <div>
-      <p>
-        Github User:{" "}
-        <a
-          href={user.resourcePath}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {user.login}
-        </a>
-      </p>
-      <p>
-        <img src={user.avatarUrl} alt={user.login} />
-      </p>
-      <p>Name: {user.name}</p>
-      <p>Company: {user.company || "None"}</p>
-      <p>Bio: {user.bio || "None"}</p>
+    <div css={styles}>
+      <div className="user-container">
+        <div className="user-img">
+          <img src={user.avatarUrl} alt={user.login} />
+        </div>
+        <div className="user-info">
+          <p>
+            <span>Name:</span> {user.name} (
+            <a
+              href={user.resourcePath}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              @{user.login}
+            </a>
+            )
+          </p>
+          <p>
+            <span>Company:</span> {user.company || "None"}
+          </p>
+          <p>
+            <span>Bio:</span> {user.bio || "None"}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
