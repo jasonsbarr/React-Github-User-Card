a45f742c8cc5249ca52fdb0e0022238bff37b334
Jason Barr <jason@jasonsbarr.com>
2019-11-06 08:43:24 -0600

    Refactor to function; Add global styles

--- github-user-card-classes/src/components/App/index.js
+++ github-user-card-classes/src/components/App/index.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import { ApolloConsumer } from "react-apollo";
 import Viewer from "../Viewer";
+import styles from "./styles";
 
-class App extends Component {
-  render() {
-    return (
-      <ApolloConsumer>
-        {client => <Viewer client={client} />}
-      </ApolloConsumer>
-    );
-  }
-}
+const App = () => (
+  <div css={styles}>
+    <ApolloConsumer>
+      {client => <Viewer client={client} />}
+    </ApolloConsumer>
+  </div>
+);
 
 export default App;
---
+++ github-user-card-classes/src/components/App/styles.js
@@ -0,0 +1,40 @@
+import { css } from "@emotion/core";
+
+const styles = css`
+  align-items: center;
+  border: 3px solid #dddddd;
+  border-radius: 5px;
+  background: #fafafa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
+    "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
+    "Helvetica Neue", Arial, sans-serif;
+  margin: 1.5rem auto;
+  max-width: 1080px;
+  padding: 1.5rem 2.5rem;
+  width: 90%;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Raleway", sans-serif;
+    font-weight: 600;
+
+    span {
+      color: #999999;
+    }
+  }
+
+  h1 {
+    span {
+      font-size: 1.5rem;
+    }
+  }
+`;
+
+export default styles;
